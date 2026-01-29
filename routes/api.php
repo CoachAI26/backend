39928bd3ab145f1f4e36e0ed0f21b1d1d5a9ab07
daniialhz <daniialhasanzade@gmail.com>
2026-01-29 11:50:55 +0330

    Auth created

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PasswordController;
+
+Route::prefix("/v1")->group(function (){
+    Route::prefix('auth')->group(function () {
+
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login',    [AuthController::class, 'login']);
+    
+        Route::post('/forgot-password', [PasswordController::class, 'forgot']);
+        Route::post('/reset-password',  [PasswordController::class, 'reset']);
+    
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [AuthController::class, 'logout']);
+    
+            Route::get('/me', function (Request $request) {
+                return $request->user();
+            });
+        });
+    });
+});
